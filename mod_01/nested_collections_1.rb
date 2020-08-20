f4d4b971ad421651d3e49d283453cec756e157f9
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-20 18:16:45 -0400

    Create nested_collections_1 practice exercises

---
+++ mod_01/nested_collections_1.rb
@@ -0,0 +1,27 @@
+# STATE CAPITALS
+states = {
+  'Oregon' => 'OR',
+  'Alabama' => 'AL',
+  'New Jersey' => 'NJ',
+  'Colorado' => 'CO'
+}
+
+capitals = {
+  'OR' => 'Salem',
+  'AL' => 'Montgomery',
+  'NJ' => 'Trenton',
+  'CO' => 'Denver'
+}
+
+# Level 1: Write some code which given a state name (“Alabama”) outputs the state abbreviation
+# Level 2: Write some code which given a state name (“Oregon”) outputs its capital (“Salem”)
+# Level 3: Handle the case when a state’s information is not known by returning “Unknown”
+# Level 4: Let’s go the other way. Given a capital name (“Denver”), return the state name for which it is the capital (“Colorado”)
+# Level 5: Write some code to turn these two hashes into one nested hash which looks like this:
+
+# state_info = {
+#   'Oregon' => { abbreviation: 'OR', capital: 'Salem' },
+#   'Alabama' => { abbreviation: 'AL', capital: 'Montgomery' },
+#   'New Jersey' => { abbreviation: 'NJ', capital: 'Trenton' },
+#   'Colorado' => { abbreviation: 'CO', capital: 'Denver' }
+# }
