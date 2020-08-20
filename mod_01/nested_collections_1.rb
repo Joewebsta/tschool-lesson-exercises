1c4d43cc4c38476eb20a3dc2ba97ae0a02a667ec
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-20 18:28:18 -0400

    Complete nested_collections problems 1-3 (of 5)

--- mod_01/nested_collections_1.rb
+++ mod_01/nested_collections_1.rb
@@ -14,8 +14,15 @@ capitals = {
 }
 
 # Level 1: Write some code which given a state name (“Alabama”) outputs the state abbreviation
+states['Alabama']
+
 # Level 2: Write some code which given a state name (“Oregon”) outputs its capital (“Salem”)
+capitals[states['Oregon']]
+
 # Level 3: Handle the case when a state’s information is not known by returning “Unknown”
+# states.fetch(:key, 'Unknown')
+# p states['Massachusetts']
+
 # Level 4: Let’s go the other way. Given a capital name (“Denver”), return the state name for which it is the capital (“Colorado”)
 # Level 5: Write some code to turn these two hashes into one nested hash which looks like this:
 
